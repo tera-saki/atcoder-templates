affd8eee9fae02bff7fdca2c8d71cf26ae34f341
tera-saki <tera_saki@icloud.com>
2022-02-21 15:13:29 +0900

    add pypyjit
    squash! add pypyjit

--- template.py
+++ template.py
@@ -1,4 +1,5 @@
 import sys
+import pypyjit
 import itertools
 import heapq
 import math
@@ -6,3 +7,4 @@ from collections import deque, defaultdict, Counter
 
 input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 6)
+pypyjit.set_param('max_unroll_recursion=-1')
