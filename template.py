853ec39cdc38e519a27d7cd54346bec6750b37a2
tera-saki <tera_saki@icloud.com>
2022-06-05 01:15:29 +0900

    feat: add binary search utility

--- template.py
+++ template.py
@@ -3,8 +3,29 @@ import pypyjit
 import itertools
 import heapq
 import math
-from collections import deque, defaultdict, Counter
+from collections import deque, defaultdict
+import bisect
 
 input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 6)
 pypyjit.set_param('max_unroll_recursion=-1')
+
+
+def index_lt(a, x):
+    'return largest index s.t. A[i] < x or -1 if it does not exist'
+    return bisect.bisect_left(a, x) - 1
+
+
+def index_le(a, x):
+    'return largest index s.t. A[i] <= x or -1 if it does not exist'
+    return bisect.bisect_right(a, x) - 1
+
+
+def index_gt(a, x):
+    'return smallest index s.t. A[i] > x or len(a) if it does not exist'
+    return bisect.bisect_right(a, x)
+
+
+def index_ge(a, x):
+    'return smallest index s.t. A[i] >= x or len(a) if it does not exist'
+    return bisect.bisect_left(a, x)
