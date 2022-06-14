f199d512a3a4a3ab852d248aecc83b0196cda884
tera-saki <tera_saki@icloud.com>
2022-06-14 22:08:24 +0900

    fix: 0H0 = 1
    squash! fix: 0H0 = 1

--- combination.py
+++ combination.py
@@ -1,7 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+
 class Combination:
-    def __init__(self, N, p):
+    def __init__(self, N, mod):
         self.N = N
-        self.p = p
+        self.mod = mod
         self.f = [None] * (N + 1)
         self.finv = [None] * (N + 1)
         self.inv = [None] * (N + 1)
@@ -12,25 +16,44 @@ class Combination:
         self.finv[1] = 1
         self.inv[1] = 1
         for i in range(2, N + 1):
-            self.f[i] = self.f[i - 1] * i % p
-            self.inv[i] = p - self.inv[p % i] * (p // i) % p
-            self.finv[i] = self.finv[i - 1] * self.inv[i] % p
+            self.f[i] = self.f[i - 1] * i % self.mod
+            self.inv[i] = self.mod - self.inv[self.mod % i] * (self.mod // i) % self.mod
+            self.finv[i] = self.finv[i - 1] * self.inv[i] % self.mod
 
     def P(self, n, k):
         if n < k:
             return 0
         if n < 0 or k < 0:
             return 0
-        return self.f[n] * self.finv[n - k] % self.p
+        return self.f[n] * self.finv[n - k] % self.mod
 
     def C(self, n, k):
         if n < k:
             return 0
         if n < 0 or k < 0:
             return 0
-        return self.f[n] * (self.finv[k] * self.finv[n - k] % self.p) % self.p
+        return self.f[n] * (self.finv[k] * self.finv[n - k] % self.mod) % self.mod
 
     # 重複組合せ
     # n種類のものからk個選ぶ
     def H(self, n, k):
+        if n == 0 and k == 0:
+            return 1
         return self.C(k + n - 1, k)
+
+
+# https://yukicoder.me/problems/no/117
+T = int(input())
+mod = 10 ** 9 + 7
+comb = Combination(2 * 10 ** 6, mod)
+
+for _ in range(T):
+    query = input()[:-1]
+    t = query[0]
+    n, k = map(int, query[2:-1].split(','))
+    if t == 'C':
+        print(comb.C(n, k))
+    elif t == 'P':
+        print(comb.P(n, k))
+    else:
+        print(comb.H(n, k))
