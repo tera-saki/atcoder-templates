82f79b411ef31af2450694a45667f9fca8c0db24
tera-saki <tera_saki@icloud.com>
2022-06-26 02:16:16 +0900

    feat: add option whether array should be strictly increasing or not

--- lis.py
+++ lis.py
@@ -1,4 +1,5 @@
 import sys
+from operator import lt, le
 input = sys.stdin.readline
 
 
@@ -7,20 +8,21 @@ class LIS:
         self.N = N
         self.A = A
 
-    def solve(self):
+    def solve(self, strict=True):
         S = [self.A[0]]
+        cmp = lt if strict else le
         for i in range(1, self.N):
-            if self.A[i] > S[-1]:
+            if cmp(S[-1], self.A[i]):
                 S.append(self.A[i])
             else:
                 l = -1
                 r = len(S)
                 while r - l > 1:
                     c = (l + r) // 2
-                    if S[c] >= self.A[i]:
-                        r = c
-                    else:
+                    if cmp(S[c], self.A[i]):
                         l = c
+                    else:
+                        r = c
                 S[r] = self.A[i]
         return len(S)
 
