37308182e3e582b2af379cfb8eeb864193be4380
GitHub <noreply@github.com>
2022-09-04 12:37:39 +0000

    [auto-verifier] verify commit 8a4df099914d739db9efd976ca6b38aaeac5a0a4
    squash! [auto-verifier] verify commit 8a4df099914d739db9efd976ca6b38aaeac5a0a4

--- topological_sort.test.py
+++ topological_sort.test.py
@@ -1,5 +1,5 @@
 # verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/GRL_4_B
-# verification-helper IGNORE
+# verification-helper: IGNORE
 # https://onlinejudge.u-aizu.ac.jp/status/users/terasa/submissions/1/GRL_4_B/judge/6940421/Python3
 import sys
 from topological_sort import TopologicalSort
