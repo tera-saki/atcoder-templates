66537dae1b4d9ef7f369987efd1a6f43641bf942
tera-saki <tera_saki@icloud.com>
2022-09-05 18:30:36 +0900

    test: add test for rolling hash

--- lib/rolling_hash.py
+++ lib/rolling_hash.py
@@ -1,16 +1,14 @@
-import sys
 import random
-
-input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 6)
 
 
 class RollingHash:
-    def __init__(self, S):
+    def __init__(self, S, mod=None, base=None):
         self.h = [0] * (len(S) + 1)
         self.p = [1] * (len(S) + 1)
-        self.mod = (1 << 61) - 1
-        self.base = random.randint(1, self.mod - 1)
+        if mod is None:
+            self.mod = (1 << 61) - 1
+        if base is None:
+            self.base = random.randint(1, self.mod - 1)
 
         for i in range(len(S)):
             self.h[i + 1] = (self.h[i] * self.base + ord(S[i])) % self.mod
@@ -20,35 +18,3 @@ class RollingHash:
     # S[l:r]
     def get(self, l, r):
         return (self.h[r] - self.h[l] * self.p[r - l]) % self.mod
-
-
-# https://atcoder.jp/contests/abc141/tasks/abc141_e
-N = int(input())
-S = input()[:-1]
-
-RH = RollingHash(S)
-
-
-def judge(length):
-    hd = {}
-    for i in range(N):
-        if i + length > N:
-            break
-        h = RH.get(i, i + length)
-        if hd.get(h) is not None:
-            if hd[h] + length <= i:
-                return True
-        else:
-            hd[h] = i
-    return False
-
-
-l = 0
-r = len(S) + 1
-while r - l > 1:
-    c = (l + r) // 2
-    if judge(c) is True:
-        l = c
-    else:
-        r = c
-print(l)
---
+++ tests/rolling_hash.test.py
@@ -0,0 +1,23 @@
+# verification-helper: https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_14_B
+import sys
+import random
+from lib.rolling_hash import RollingHash
+
+input = sys.stdin.readline
+
+
+T = input()[:-1]
+P = input()[:-1]
+
+mod = (1 << 61) - 1
+base = random.randint(1, mod - 1)
+RT = RollingHash(T, mod=mod, base=base)
+RP = RollingHash(P, mod=mod, base=base)
+
+target = RP.get(0, len(P))
+for i in range(len(T)):
+    if i + len(P) > len(T):
+        break
+    h = RT.get(i, i + len(P))
+    if h == target:
+        print(i)
