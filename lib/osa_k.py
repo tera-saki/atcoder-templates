94e357d03dff81d12ea1414d86e3e706f21d7c91
tera-saki <tera_saki@icloud.com>
2022-09-05 23:29:15 +0900

    test: add test for osa_k

--- lib/osa_k.py
+++ lib/osa_k.py
@@ -1,7 +1,3 @@
-import sys
-from collections import defaultdict
-
-
 class Osa_k:
     # N以下の整数を素因数分解 O(NlogN)
     def __init__(self, N):
@@ -22,25 +18,3 @@ class Osa_k:
             f.append(self.min_factor[n])
             n //= self.min_factor[n]
         return f
-
-
-# https://yukicoder.me/problems/no/1006
-X = int(input())
-osa_k = Osa_k(X)
-f = [0] * X
-for i in range(1, X):
-    factors = osa_k.factors(i)
-    D = defaultdict(int)
-    for factor in factors:
-        D[factor] += 1
-    d = 1
-    for v in D.values():
-        d *= (v + 1)
-    f[i] = i - d
-v = [1 << 30] * X
-for i in range(1, X):
-    v[i] = abs(f[i] - f[X - i])
-minv = min(v)
-for i in range(1, X):
-    if v[i] == minv:
-        print(i, X - i)
---
+++ tests/osa_k.test.py
@@ -0,0 +1,26 @@
+# verification-helper: PROBLEM https://yukicoder.me/problems/no/1006
+import sys
+from collections import defaultdict
+from lib.osa_k import Osa_k
+
+input = sys.stdin.readline
+
+X = int(input())
+osa_k = Osa_k(X)
+f = [0] * X
+for i in range(1, X):
+    factors = osa_k.factors(i)
+    D = defaultdict(int)
+    for factor in factors:
+        D[factor] += 1
+    d = 1
+    for v in D.values():
+        d *= (v + 1)
+    f[i] = i - d
+v = [1 << 30] * X
+for i in range(1, X):
+    v[i] = abs(f[i] - f[X - i])
+minv = min(v)
+for i in range(1, X):
+    if v[i] == minv:
+        print(i, X - i)
