59d51dc87b59742f4bc1d06219a9eee392ba8361
tera-saki <tera_saki@icloud.com>
2022-09-25 12:34:01 +0900

    refactor: add type annotation to two_sat

--- library/two_sat.py
+++ library/two_sat.py
@@ -1,12 +1,14 @@
+from typing import List
+
 from library.scc import SCC
 
 
 class TwoSat:
-    def __init__(self, N):
+    def __init__(self, N: int):
         self.N = N
         self.E = [[] for _ in range(2 * N)]
 
-    def add_clause(self, s, t):
+    def add_clause(self, s: int, t: int) -> None:
         def vid(n):
             return 2 * (~n) + 1 if n < 0 else 2 * n
 
@@ -19,7 +21,7 @@ class TwoSat:
         # ~t -> s
         self.E[oppose(tid)].append(sid)
 
-    def solve(self):
+    def solve(self) -> List[bool]:
         cid = SCC(2 * self.N, self.E).cid
         for i in range(self.N):
             if cid[2 * i] == cid[2 * i + 1]:
