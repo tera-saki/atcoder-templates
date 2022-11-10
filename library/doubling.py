66f2095bb551991f6378846eb07659c8823daa13
tera-saki <tera_saki@icloud.com>
2022-11-10 20:58:44 +0900

    feat: add DoublingMonoid

--- library/doubling.py
+++ library/doubling.py
@@ -15,3 +15,31 @@ class Doubling:
                 p = self.d[i][p]
             k >>= 1
         return p
+
+
+class DoublingMonoid:
+    def __init__(self, nex, A, op, e, digit=60):
+        self.op = op
+        self.e = e
+        self.digit = digit
+
+        self.L = len(nex)
+        self.d = [[None for _ in range(self.L)] for _ in range(digit)]
+        self.S = [[e for _ in range(self.L)] for _ in range(digit)]
+
+        self.d[0] = nex
+        self.S[0] = A
+        for i in range(1, self.digit):
+            for j in range(self.L):
+                self.d[i][j] = self.d[i - 1][self.d[i - 1][j]]
+                self.S[i][j] = self.op(self.S[i - 1][j], self.S[i - 1][self.d[i - 1][j]])
+
+    def solve(self, start, k):
+        p = start
+        acc = self.e
+        for i in range(self.digit):
+            if k & 1:
+                acc = self.op(acc, self.S[i][p])
+                p = self.d[i][p]
+            k >>= 1
+        return acc
